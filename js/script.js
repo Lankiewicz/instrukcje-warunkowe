dab507ce64a8096e4ec9ba3f2831b8798b2b23d7
Jakub Lankiewicz <lankiewiczjakub@gmail.com>
2018-02-26 21:28:45 +0100

    add code to js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var a= 2,
+b= 3,
+value= -17;
+console.log(value);
+var check = value > 0 ? 'wynik jest dodatni' : 'wynik jest ujemny';
+console.log(check);
+var check = value = 0 ? 'wynik równy zero' : 'wynik nie równy zero';
+console.log(check);
